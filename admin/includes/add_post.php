e6e808aa500817447704d7d88ba2d46f13431340
ruldane <l.nizzoli@gmail.com>
2022-09-13 23:35:28 +0200

    add crud post

---
+++ admin/includes/add_post.php
@@ -0,0 +1,62 @@
+<?php
+if (isset($_POST['create_post'])) {
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+
+
+    if (isset($_FILES['image'])) {
+        $tmpName = $_FILES['image']['tmp_name'];
+        $post_image = $_FILES['image']['name'];
+
+        move_uploaded_file($tmpName, '../images/' . $post_image);
+    }
+
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date,post_image,post_content,post_tags,post_status, post_comment_count) ";
+
+    $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}', '{$post_comment_count}') ";
+
+    $create_category_query = $database->query($query);
+}
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title" />
+    </div>
+    <div class="form-group">
+        <label for="post_category">Post Category Id</label>
+        <input type="text" class="form-control" name="post_category_id" />
+    </div>
+    <div class="form-group">
+        <label for="author">Post Author</label>
+        <input type="text" class="form-control" name="author" />
+    </div>
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input type="text" class="form-control" name="post_status" />
+    </div>
+    <div class="form-group">
+        <label for="file">Image</label>
+        <input type="file" name="image">
+    </div>
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags" />
+    </div>
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post" />
+    </div>
+</form>
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -39,15 +39,15 @@
                     Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">Vieuw All Posts</a>
+                        <a href="posts.php">Vieuw All Posts</a>
                     </li>
                     <li>
-                        <a href="#">Add Post</a>
+                        <a href="posts?source=add_post">Add Post</a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
             </li>
 
             <li class="active">
